cd39f629ae83e27a9fd602f5e318d496a96e14f0
yoons0717 <yoons0717@gmail.com>
2020-09-03 19:47:38 +0900

    set up react & redux  develop env

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React , {useEffect} from 'react';
+import axios from 'axios';
+
+function LandingPage(){
+
+    useEffect(()=>{
+        axios.get('/api/hello')
+        .then(response=>{ console.log(response)})
+    },[])
+
+    return(
+        <div>
+            LandingPage 랜딩
+        </div>
+    )
+}
+
+export default LandingPage
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express') // express 모듈을 가져옴
 const app = express()              // 앱 
-const port = 5000
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 
@@ -26,6 +26,10 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => { // 루트 디렉토리에
   res.send('Hello World! 헬로')
+})
+
+app.get('/api/hello', (req,res) => {
+    res.send("안녕하세요 ~ ")
 })
 
 // 회원가입을 위한 라우트 
@@ -106,6 +110,10 @@ app.get('/api/users/logout', auth, (req,res)=>{
 })
 
 
+
+
+
+const port = 5000
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 }) 
